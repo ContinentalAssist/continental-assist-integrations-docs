05d0b67d70330df393aa0555a7eb96316f080967
ErnestoContinental <egalvan@continentalassist.com>
2022-09-29 15:01:59 -0500

    Ajustes actualizados

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,9 @@ project = 'Continental Assist'
 copyright = '2022, Continental Assist'
 author = 'Continental Assist'
 
+release = '0.1'
+version = '0.1.0'
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -21,6 +24,12 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -31,3 +40,6 @@ language = 'es'
 
 html_theme = 'sphinx_rtd_theme'
 # html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
